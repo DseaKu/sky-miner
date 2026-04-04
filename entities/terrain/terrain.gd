f077b11431473ab07cf0f4b23f0c137d99c85619
Volker Kuntner <git.dsea@tutanota.com>
2026-04-04 12:55:57 +0200

    start adding tilemap

--- entities/terrain/terrain.gd
+++ entities/terrain/terrain.gd
@@ -1,11 +1,35 @@
-extends Node
+extends Node2D
 
+@onready var tile_map = $TileMapLayer
 
-# Called when the node enters the scene tree for the first time.
+const WIDTH = 100
+const DEPTH = 50
+
+# These IDs correspond to your TileSet configuration.
+# 0 is the ID of the texture atlas.
+const TILE_SOURCE_ID = 0 
+const DIRT_ATLAS_COORD = Vector2i(0, 0)
+const STONE_ATLAS_COORD = Vector2i(1, 0)
+const ORE_ATLAS_COORD = Vector2i(2, 0)
+
 func _ready() -> void:
-	pass # Replace with function body.
+	generate_terrain()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func generate_terrain() -> void:
+	for x in range(WIDTH):
+		for y in range(DEPTH):
+			# Offset the generation so it starts below the player
+			var grid_position = Vector2i(x, y + 5) 
+			
+			var block_type = DIRT_ATLAS_COORD
+			
+			# Basic depth logic: Dirt on top, stone below
+			if y > 5:
+				block_type = STONE_ATLAS_COORD
+				
+				# Random chance for ore inside the stone layer
+				if randf() < 0.05: # 5% chance
+					block_type = ORE_ATLAS_COORD
+					
+			# Place the tile: set_cell(coords, source_id, atlas_coords)
+			tile_map.set_cell(grid_position, TILE_SOURCE_ID, block_type)
