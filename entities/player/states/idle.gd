9a76516ee3ee050b34e74522da98f9709ebcdd6c
Volker Kuntner <git.dsea@tutanota.com>
2026-04-04 15:56:26 +0200

    add mine

--- entities/player/states/idle.gd
+++ entities/player/states/idle.gd
@@ -6,6 +6,10 @@ func enter():
 func update(delta: float):
 	handle_gravity(delta)
 	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		actor.state_machine.transition_to("mine")
+		return
+		
 	if Input.is_action_just_pressed("ui_accept") and actor.is_on_floor():
 		actor.state_machine.transition_to("jump")
 		return
--- entities/player/states/mine.gd
+++ entities/player/states/mine.gd
@@ -7,4 +7,23 @@ func enter():
 
 func update(delta: float):
 	handle_gravity(delta)
+	
+	if mining_timer > 0:
+		mining_timer -= delta
+		
+	if not actor.is_on_floor():
+		actor.state_machine.transition_to("fall")
+		return
 
+	if mining_timer <= 0:
+		var mouse_pos = actor.get_global_mouse_position()
+		if actor.global_position.distance_to(mouse_pos) <= actor.MINING_RANGE:
+			if actor.terrain:
+				if actor.terrain.mine_tile(mouse_pos):
+					mining_timer = actor.MINING_COOLDOWN
+
+	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		actor.state_machine.transition_to("idle")
+		return
+
+	
