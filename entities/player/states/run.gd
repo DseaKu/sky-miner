b0d164c18f39bef694d58f693904dcf65f1e2764
Volker Kuntner <git.dsea@tutanota.com>
2026-04-05 13:26:14 +0200

    improve land

--- entities/player/states/run.gd
+++ entities/player/states/run.gd
@@ -30,10 +30,9 @@ func handle_transitions(_delta: float):
 		return
 
 	if not(Input.is_action_pressed("left") or Input.is_action_pressed("right")):
-		actor.state_machine.transition_to("idle")
-
 		if Input.is_action_pressed("mine"):
 			actor.state_machine.transition_to("mine")
 			return
 
+		actor.state_machine.transition_to("idle")
 		return
--- ui/ui.gd
+++ ui/ui.gd
@@ -1,11 +1,32 @@
-extends Node2D
+extends CanvasLayer
 
+# Create references to your labels
+@onready var fps_label = $FPSLabel
+@onready var pos_label = $PosLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	visible =false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# func _input(event: InputEvent) -> void:
+# 	if event.is_action_just_pressed("show_debug_ui"):
+# 		visible = not visible
+
+func _process(_delta: float) -> void:
+	# Do not process updates if the UI is hidden
+	if not visible:
+		return
+		
+	# Update FPS
+	fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
+	
+	# Update Player Position
+	var player = get_tree().get_first_node_in_group("player")
+	
+	if player:
+		var pos_x = round(player.global_position.x)
+		var pos_y = round(player.global_position.y)
+		pos_label.text = "Pos: (" + str(pos_x) + ", " + str(pos_y) + ")"
+	else:
+		pos_label.text = "Pos: Player not found"
