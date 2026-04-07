dc5fa34c260699d488a4d8140eba58ed11a0ea25
Volker Kuntner <git.dsea@tutanota.com>
2026-04-07 11:47:59 +0200

    fix state naming

--- scripts/state_machine.gd
+++ scripts/state_machine.gd
@@ -5,30 +5,33 @@ extends Node
 var current_state: State
 var states: Dictionary = {}
 
+
 func _ready():
-    await get_parent().ready
-    for child in get_children():
-        if child.has_method("enter"):
-            states[child.name.to_lower()] = child
-            child.actor = get_parent()
-    
-    if initial_state:
-        current_state = initial_state
-        current_state.enter()
+	await get_parent().ready
+	for child in get_children():
+		if child.has_method("enter"):
+			var key = child.name.to_snake_case()
+			states[key] = child
+			child.actor = get_parent()
+
+	if initial_state:
+		current_state = initial_state
+		current_state.enter()
+
 
 func _physics_process(delta):
-    if current_state:
-        current_state.handle_transitions(delta)
-        current_state.physics_update(delta)
+	if current_state:
+		current_state.handle_transitions(delta)
+		current_state.physics_update(delta)
+
 
 func transition_to(state_name: String):
-    # print("Transitioning to: ", state_name)
-    var new_state = states.get(state_name.to_lower())
-    if !new_state:
-        return
-        
-    if current_state:
-        current_state.exit()
-        
-    current_state = new_state
-    current_state.enter()
+	var new_state = states.get(state_name)
+	if !new_state:
+		return
+
+	if current_state:
+		current_state.exit()
+
+	current_state = new_state
+	current_state.enter()
