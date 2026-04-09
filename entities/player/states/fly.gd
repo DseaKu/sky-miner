36140d1a97b5de302e2b824de5d5f24017cbfecf
Volker Kuntner <git.dsea@tutanota.com>
2026-04-09 08:32:54 +0200

    remove fly dup

--- entities/player/states/fly.gd
+++ entities/player/states/fly.gd
@@ -3,19 +3,27 @@ extends PlayerState
 
 func enter():
 	actor.animation_player.play("idle")
+	actor.get_node("CollisionShape2D").set_deferred("disabled", true)
+
+
+func exit():
+	actor.get_node("CollisionShape2D").set_deferred("disabled", false)
 
 
 func physics_update(_delta: float):
 	if Input.is_action_pressed("up"):
-		actor.velocity.y = -600
+		actor.velocity.y = -actor.FLYING_SPEED
+
+	elif Input.is_action_pressed("down"):
+		actor.velocity.y = actor.FLYING_SPEED
 	else:
 		actor.velocity.y = 0
 
 	if Input.is_action_pressed("left"):
-		actor.velocity.x = -600
+		actor.velocity.x = -actor.FLYING_SPEED
 
 	elif Input.is_action_pressed("right"):
-		actor.velocity.x = 600
+		actor.velocity.x = actor.FLYING_SPEED
 	else:
 		actor.velocity.x = 0
 
@@ -23,6 +31,6 @@ func physics_update(_delta: float):
 
 
 func handle_transitions(_delta: float):
-	if Input.is_action_just_pressed("down"):
+	if Input.is_action_just_pressed("jump"):
 		actor.state_machine.transition_to("idle")
 		return
