e24f27d5d0e2d5555a66d71fc50f210e63065f7a
Volker Kuntner <git.dsea@tutanota.com>
2026-05-17 20:21:14 +0200

    refine map interface

--- rust/src/map/node_interface.rs
+++ rust/src/map/node_interface.rs
@@ -1,4 +1,4 @@
-use super::consts::path::*;
+use super::consts::path;
 use super::generator;
 use godot::classes::TileMapLayer;
 use godot::prelude::*;
@@ -15,7 +15,7 @@ pub struct MapGenNode {
 #[godot_api]
 impl INode for MapGenNode {
     fn init(base: Base<Node>) -> Self {
-        godot_print!("{}Initializing...", MAP_GEN_NODE_STR);
+        godot_print!("{}Initializing...", path::MAP_GEN_NODE_STR);
         Self {
             player_node: None,
             tile_map_node: None,
@@ -26,26 +26,29 @@ impl INode for MapGenNode {
 
     fn ready(&mut self) {
         // Try to fetch Player Node
-        if let Some(player) = self.base().try_get_node_as::<Node2D>(PLAYER_NODE_PATH) {
+        if let Some(player) = self
+            .base()
+            .try_get_node_as::<Node2D>(path::PLAYER_NODE_PATH)
+        {
             let mut player_to_connect = player.clone();
             player_to_connect.connect(
                 "tree_exiting",
                 &self.base().callable("on_player_tree_exiting"),
             );
             self.player_node = Some(player);
-            godot_print!("{}Linked to Player node", MAP_GEN_NODE_STR);
+            godot_print!("{}Linked to Player node", path::MAP_GEN_NODE_STR);
         } else {
             godot_warn!(
                 "{}Could not fetch Player node at {}",
-                MAP_GEN_NODE_STR,
-                PLAYER_NODE_PATH
+                path::MAP_GEN_NODE_STR,
+                path::PLAYER_NODE_PATH
             );
         }
 
         // Try to fetch Tile Map Node
         if let Some(tile_map) = self
             .base()
-            .try_get_node_as::<TileMapLayer>(TILE_MAP_LAYER_NODE_PATH)
+            .try_get_node_as::<TileMapLayer>(path::TILE_MAP_LAYER_NODE_PATH)
         {
             let mut tile_map_to_connect = tile_map.clone();
             tile_map_to_connect.connect(
@@ -53,12 +56,12 @@ impl INode for MapGenNode {
                 &self.base().callable("on_tile_map_layer_exiting"),
             );
             self.tile_map_node = Some(tile_map);
-            godot_print!("{}Linked to TileMapLayer node", MAP_GEN_NODE_STR);
+            godot_print!("{}Linked to TileMapLayer node", path::MAP_GEN_NODE_STR);
         } else {
             godot_warn!(
                 "{}Could not fetch TileMapLayer node at {}",
-                MAP_GEN_NODE_STR,
-                TILE_MAP_LAYER_NODE_PATH
+                path::MAP_GEN_NODE_STR,
+                path::TILE_MAP_LAYER_NODE_PATH
             );
         }
     }
@@ -76,7 +79,7 @@ impl MapGenNode {
     fn on_player_tree_exiting(&mut self) {
         godot_print!(
             "{}Player node exiting tree. Stopping processing.",
-            MAP_GEN_NODE_STR
+            path::MAP_GEN_NODE_STR
         );
         self.player_node = None;
         self.base_mut().set_process(false);
@@ -86,7 +89,7 @@ impl MapGenNode {
     fn on_tile_map_layer_exiting(&mut self) {
         godot_print!(
             "{}Tile Map Layer node exiting tree. Stopping processing.",
-            MAP_GEN_NODE_STR
+            path::MAP_GEN_NODE_STR
         );
         self.tile_map_node = None;
         self.base_mut().set_process(false);
@@ -103,12 +106,10 @@ impl MapGenNode {
 
     fn get_player_grid_pos(&self) -> Option<Vector2i> {
         if let (Some(player), Some(tile_map)) = (&self.player_node, &self.tile_map_node) {
-            if player.is_instance_valid() && tile_map.is_instance_valid() {
-                let global_pos = player.get_global_position();
-                let local_pos = tile_map.to_local(global_pos);
-                let grid_pos = tile_map.local_to_map(local_pos);
-                return Some(grid_pos);
-            }
+            let global_pos = player.get_global_position();
+            let local_pos = tile_map.to_local(global_pos);
+            let grid_pos = tile_map.local_to_map(local_pos);
+            return Some(grid_pos);
         }
         None
     }
